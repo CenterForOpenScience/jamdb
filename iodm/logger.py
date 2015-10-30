71c1b50b926a884561170bf3de9281f18e1be0e9
Chris Seto <chriskseto@gmail.com>
2015-10-30 12:21:55 -0400

    Seperate Logger into Logger and ReadOnlyLogger

--- iodm/logger.py
+++ iodm/logger.py
@@ -7,29 +7,16 @@ from iodm import O
 from iodm.base import Log
 from iodm.base import LogSchema
 from iodm.base import Operation
-from iodm.backends.translation import TranslatingBackend
+from iodm.backends.ext import TranslatingBackend
+from iodm.backends.ext import ReadOnlyFilteredBackend
 
 
-class Logger:
+class ReadOnlyLogger:
 
     __version__ = '0.0.0'
 
     def __init__(self, backend):
         self._backend = TranslatingBackend(Log, LogSchema, backend)
-
-    def create(self, operation, key, data_ref, **operation_parameters):
-        log = Log(
-            ref=str(bson.ObjectId()),
-            record_id=key,
-            data_ref=data_ref,
-            timestamp=time.time(),
-            version=self.__version__,
-            operation=Operation(operation),
-            operation_parameters=operation_parameters
-        )
-
-        self._backend.set(log.ref, log)
-        return log
 
     def get(self, ref):
         return self._backend.get(ref)
@@ -48,3 +35,34 @@ class Logger:
 
     def list(self, order=None):
         return self._backend.list(order)
+
+
+class Logger(ReadOnlyLogger):
+
+    def create(self, operation, key, data_ref, **operation_parameters):
+        log = Log(
+            ref=str(bson.ObjectId()),
+            record_id=key,
+            data_ref=data_ref,
+            timestamp=time.time(),
+            version=self.__version__,
+            operation=Operation(operation),
+            operation_parameters=operation_parameters
+        )
+
+        self._backend.set(log.ref, log)
+        return log
+
+    def at_time(self, timestamp):
+        return FrozenLogger(timestamp, self._backend._backend)
+
+
+class FrozenLogger(ReadOnlyLogger):
+    """A Read-only logger that limits access to logs before the given timestamp"""
+
+    def __init__(self, timestamp, backend):
+        super().__init__(backend)
+        self._backend = ReadOnlyFilteredBackend(
+            Q('timestamp', 'eq', timestamp),
+            self._backend
+        )
