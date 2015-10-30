3ce648f1f78d7818f04695a0c3c037f568877671
Chris Seto <chriskseto@gmail.com>
2015-10-30 14:17:07 -0400

    Implement composable queries

--- iodm/backends/util.py
+++ iodm/backends/util.py
@@ -14,8 +14,18 @@ class Query:
     def as_lambda(self):
         comparator = getattr(operator, self.comparator)
         return lambda val: comparator(val[self.key], self.value)
-        # getter = operator.attrgetter(self.key)
-        # return lambda val: comparator(getter(val), self.value)
+
+    def __and__(self, other):
+        return CompoundQuery(self, other)
+
+
+class CompoundQuery:
+
+    def __init__(self, *queries):
+        self.queries = queries
+
+    def as_lambda(self):
+        return lambda val: all(q.as_lambda()(val) for q in self.queries)
 
 
 class Order:
