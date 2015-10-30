fbd5e3ae4118ef251a4ca5a9cecc2acac36742c3
Chris Seto <chriskseto@gmail.com>
2015-10-30 14:23:59 -0400

    Update example

--- examples/timemachine.py
+++ examples/timemachine.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     logger = Logger(EphemeralBackend())
     storage = Storage(EphemeralBackend())
     snapshot = Snapshot(EphemeralBackend())
-    collection = Collection(storage, logger, snapshot, regenerate=False)
+    collection = Collection(storage, logger, snapshot)
 
     ts2012 = calendar.timegm(datetime.datetime(year=2012, month=1, day=14).timetuple())
     ts2013 = calendar.timegm(datetime.datetime(year=2013, month=1, day=14).timetuple())
@@ -25,6 +25,7 @@ if __name__ == '__main__':
 
     with freeze_time('2013-01-14'):
         collection.create('key1', 'value20130114')
+        snapshot = collection.snapshot()
 
     with freeze_time('2014-01-14'):
         collection.create('key1', 'value20140114')
@@ -51,3 +52,18 @@ if __name__ == '__main__':
         key = None
 
     assert key is None
+
+    collection = Collection(storage, logger, Snapshot(EphemeralBackend()), regenerate=False)
+    collection.load_snapshot(snapshot)
+
+    assert collection.read('key1').data == 'value20130114'
+    try:
+        key = collection.read('2014key')
+    except Exception:
+        key = None
+
+    assert key is None
+
+    collection.regenerate()
+
+    assert collection.read('2014key').data == 'value'
