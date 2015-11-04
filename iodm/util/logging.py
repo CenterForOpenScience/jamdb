445d0e0303679345002cb74ba357b49acaaf4d9d
Chris Seto <chriskseto@gmail.com>
2015-11-04 14:47:09 -0500

    Color logging

---
+++ iodm/util/logging.py
@@ -0,0 +1,43 @@
+import logging
+import logging.config
+
+try:
+    import colorlog  # noqa
+    DEFAULT_FORMATTER = {
+        '()': 'colorlog.ColoredFormatter',
+        'format': '%(cyan)s[%(asctime)s]%(log_color)s[%(levelname)s][%(name)s]: %(reset)s%(message)s'
+    }
+except ImportError:
+    DEFAULT_FORMATTER = {
+        '()': 'waterbutler.core.logging.MaskFormatter',
+        'format': '[%(asctime)s][%(levelname)s][%(name)s]: %(message)s',
+        'pattern': '(?<=cookie=)(.*?)(?=&|$)',
+        'mask': '***'
+    }
+
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': DEFAULT_FORMATTER,
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'INFO',
+            'formatter': 'console'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False
+        }
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console']
+    }
+})
