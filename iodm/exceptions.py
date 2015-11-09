50892a538f5093cae8762faa5db69b8972d8497f
Chris Seto <chriskseto@gmail.com>
2015-11-09 16:49:16 -0500

    Make exceptions http serializable

--- iodm/exceptions.py
+++ iodm/exceptions.py
@@ -1,5 +1,23 @@
+import json
+import http.client
+
+
 class IodmException(Exception):
-    pass
+    def __init__(self, message, code=http.client.INTERNAL_SERVER_ERROR):
+        super().__init__(code)
+        self.code = code
+        if isinstance(message, dict):
+            self.data = message
+            self.message = json.dumps(message)
+        else:
+            self.data = None
+            self.message = message
+
+    def __repr__(self):
+        return '<{}({}, {})>'.format(self.__class__.__name__, self.code, self.message)
+
+    def __str__(self):
+        return '{}, {}'.format(self.code, self.message)
 
 
 class BackendException(IodmException):
@@ -7,4 +25,18 @@ class BackendException(IodmException):
 
 
 class NotFound(BackendException):
-    pass
+
+    def __init__(self, message=None):
+        super().__init__(message or 'Resource not found', http.client.NOT_FOUND)
+
+
+class InsufficientPermissions(IodmException):
+
+    def __init__(self, message=None):
+        super().__init__(message or 'You do not have sufficient permissions to access this resource', http.client.FORBIDDEN)
+
+
+class Unauthorized(IodmException):
+
+    def __init__(self, message=None):
+        super().__init__(message or 'Unauthorized', http.client.UNAUTHORIZED)
