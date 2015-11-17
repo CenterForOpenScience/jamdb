e43449d27515f6e89755443473f305ba73dde1fd
Chris Seto <chriskseto@gmail.com>
2015-11-17 11:50:33 -0500

    Add the search resource

--- iodm/server/api/v1/__init__.py
+++ iodm/server/api/v1/__init__.py
@@ -1,4 +1,5 @@
-from iodm.server.api.v1.auth import AuthHandler
+from iodm.server.api.v1.auth import AuthHandler  # noqa
+from iodm.server.api.v1.search import SearchResource
 from iodm.server.api.v1.history import HistoryResource
 from iodm.server.api.v1.document import DocumentResource
 from iodm.server.api.v1.namespace import NamespaceResource
@@ -8,6 +9,7 @@ from iodm.server.api.v1.collection import CollectionResource
 RESOURCES = (
     NamespaceResource,
     CollectionResource,
+    SearchResource,
     DocumentResource,
     HistoryResource,
 )
