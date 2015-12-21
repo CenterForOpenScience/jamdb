9a1bc264bd92cae3e747b9f056d78017005707a4
Chris Seto <chriskseto@gmail.com>
2015-12-21 16:23:10 -0500

    Redux of collection

--- toast/app/pods/namespace/collection/document/route.js
+++ toast/app/pods/namespace/collection/document/route.js
@@ -5,7 +5,9 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
     adapterContext: Ember.inject.service(),
 
     model(params) {
+        let self = this;
         this.set('adapterContext.namespace', this.modelFor('namespace'));
-        return this.store.findRecord('collection', params.collection_id);
+        this.set('adapterContext.collection', this.modelFor('namespace.collection'));
+        return this.store.find('document', params.document_id);
     }
 });
