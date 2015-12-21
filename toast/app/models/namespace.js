4df289ebc4df731167348e83028eef8a79419c87
Chris Seto <chriskseto@gmail.com>
2015-12-21 16:23:41 -0500

    Remove has many query plugin

--- toast/app/models/namespace.js
+++ toast/app/models/namespace.js
@@ -1,12 +1,11 @@
 import DS from 'ember-data';
-import HasManyQuery from 'ember-data-has-many-query';
 
-export default DS.Model.extend(HasManyQuery.ModelMixin, {
+export default DS.Model.extend({
     permissions: DS.attr(),
     name: DS.attr('string'),
     state: DS.attr('string'),
     logger: DS.attr('string'),
     storage: DS.attr('string'),
     createdOn: DS.attr('date'),
-    collections: DS.hasMany('collection', {async: true})
+    collections: DS.hasMany('collection', {async: true}),
 });
