4b1d1c3eb61e81d3b98db2a6694711986feb8dce
Chris Seto <chriskseto@gmail.com>
2015-12-22 10:59:36 -0500

    Remove HasManyQuery

--- toast/app/models/history.js
+++ toast/app/models/history.js
@@ -1,12 +1,11 @@
 import Ember from 'ember';
 import DS from 'ember-data';
-import HasManyQuery from 'ember-data-has-many-query';
 
 
 Ember.Inflector.inflector.uncountable('history');
 
 
-export default DS.Model.extend(HasManyQuery.ModelMixin, {
+export default DS.Model.extend({
     parameters: DS.attr(),
     recordId: DS.attr('string'),
     operation: DS.attr('string'),
