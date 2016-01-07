c5951f1aa22acd89297a146cbb446b6addc16069
Chris Seto <chriskseto@gmail.com>
2016-01-07 15:44:14 -0500

    Remove deprecated scripts. See jam --help

--- regenerate.py
+++
@@ -1,8 +0,0 @@
-import jam
-
-nsm = jam.NamespaceManager()
-
-share_ns = nsm.get_namespace('SHARE')
-
-for collection_name in share_ns.keys():
-    share_ns.get_collection(collection_name).regenerate()
