c2d68b0edd40eaaac08c8bcb4238888b19dd28d9
Chris Seto <chriskseto@gmail.com>
2016-01-11 20:49:02 -0500

    Allow creation of new collections

--- toast/app/pods/components/namespace-entry/component.js
+++ toast/app/pods/components/namespace-entry/component.js
@@ -1,20 +1,42 @@
 import Ember from 'ember';
 
 const Component = Ember.Component.extend({
-    showCollections: false,
+  store: Ember.inject.service(),
+  adapterContext: Ember.inject.service(),
 
-    tagName: 'li',
-    classNames: ['treeview'],
-    classNameBindings: ['showCollections:active'],
-    actions: {
-        toggle() {
-            this.set('showCollections', !this.get('showCollections'));
-        }
+  tagName: 'li',
+  isCreating: false,
+  showCollections: false,
+  classNames: ['treeview'],
+  classNameBindings: ['showCollections:active'],
+
+  actions: {
+    toggle() {
+      this.toggleProperty('showCollections');
+    },
+    toggleCreation() {
+      this.toggleProperty('isCreating');
+      if (this.get('isCreating'))
+        this.$('input[type=text]').focus();
+    },
+    newCollection(name) {
+      this.get('store').createRecord('collection', {
+          id: name,
+          namespace: this.get('namespace')
+      }).save();
+      this.toggleProperty('isCreating');
     }
+  },
+
+  init() {
+    this._super.apply(this, arguments);
+    if (this.get('adapterContext.namespace.id') == this.namespace.id)
+      this.set('showCollections', true);
+  }
 });
 
 Component.reopenClass({
-    positionalParams: ['namespace']
+  positionalParams: ['namespace']
 });
 
 export default Component;
