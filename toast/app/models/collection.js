8b2fdfaf1c6d76f6603038b4405ee910ecd49831
Chris Seto <chriskseto@gmail.com>
2016-01-11 20:47:59 -0500

    Minor model updates

--- toast/app/models/collection.js
+++ toast/app/models/collection.js
@@ -1,7 +1,9 @@
+import Em from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    name: DS.attr('string'),
+    name: Em.computed.alias('id'),
+    // name: DS.attr('string'),
     permissions: DS.attr(),
     createdOn: DS.attr('date'),
     documents: DS.hasMany('document', {async: true}),
