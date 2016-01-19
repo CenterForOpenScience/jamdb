4b31fc0f65fb2e4bb53968143f96f462ec079140
Chris Seto <chriskseto@gmail.com>
2016-01-19 16:14:17 -0500

    Add a mixin for CORS only

--- jam/server/api/base.py
+++ jam/server/api/base.py
@@ -30,6 +30,36 @@ CORS_EXPOSE_HEADERS = [
 ]
 
 
+class CORSMixin:
+    CORS_ACCEPT_HEADERS = [
+        'Range',
+        'Content-Type',
+        'Authorization',
+        'Cache-Control',
+        'X-Requested-With',
+    ]
+
+    CORS_EXPOSE_HEADERS = [
+        'Range',
+        'Accept-Ranges',
+        'Content-Range',
+        'Content-Length',
+        'Content-Encoding',
+    ]
+
+    def set_default_headers(self):
+        self.set_header('Access-Control-Allow-Credentials', 'true')
+        self.set_header('Access-Control-Allow-Headers', ', '.join(self.CORS_ACCEPT_HEADERS))
+        self.set_header('Access-Control-Expose-Headers', ', '.join(self.CORS_EXPOSE_HEADERS))
+        self.set_header('Cache-control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.set_header('Access-Control-Allow-Origin', self.request.headers.get('Origin', '*'))
+        super().set_default_headers()
+
+    def options(self, *args, **kwargs):
+        self.set_status(204)
+        self.set_header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+
+
 class BaseAPIHandler(tornado.web.RequestHandler, SentryMixin, metaclass=abc.ABCMeta):
 
     def get_current_user(self):
