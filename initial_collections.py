6c1706b176eff4b6a06466ff1967c154021d77f5
Chris Wisecarver <cwisecarver@cos.io>
2016-01-21 15:10:00 -0500

    Renesting JSON Schema for users so that it will work.

--- initial_collections.py
+++ initial_collections.py
@@ -5,62 +5,68 @@ import jam.auth
 nsm = jam.NamespaceManager()
 
 try:
-    share_ns = nsm.create_namespace('SHARE', 'tracked-SHARE|users-scrapi')
+    share_ns = nsm.create_namespace('OPENTRIALS', 'tracked-OPENTRIALS|users-scrapi')
 except jam.exceptions.KeyExists:
-    share_ns = nsm.get_namespace('SHARE')
+    share_ns = nsm.get_namespace('OPENTRIALS')
 
 try:
-    users_col = share_ns.create_collection('share-contributor', 'tracked-SHARE|users-scrapi', permissions={
+    users_col = share_ns.create_collection('share-contributor', 'tracked-OPENTRIALS|users-scrapi', permissions={
         '*': jam.auth.Permissions.READ
     })
 except jam.exceptions.KeyExists:
     pass
 
 try:
-    share_ns.create_collection('share-data', 'tracked-SHARE|users-scrapi', permissions={
+    share_ns.create_collection('share-data', 'tracked-OPENTRIALS|users-scrapi', permissions={
         '*': jam.auth.Permissions.READ
     })
 except jam.exceptions.KeyExists:
     pass
 
 try:
-    share_ns.create_collection('contributor-curation', 'tracked-SHARE|users-scrapi', permissions={
+    share_ns.create_collection('opentrials-data', 'tracked-OPENTRIALS|users-scrapi', permissions={
+        'tracked-OPENTRIALS|users-scrapi': jam.auth.Permissions.ADMIN
+    })
+except jam.exceptions.KeyExists:
+    pass
+
+
+try:
+    share_ns.create_collection('contributor-curation', 'tracked-OPENTRIALS|users-scrapi', permissions={
         'user-osf-*': jam.auth.Permissions.CREATE
     })
 except jam.exceptions.KeyExists:
     share_ns.update('contributor-curation', [{
         'op': 'add', 'path': '/permissions/user-osf-*', 'value': jam.auth.Permissions.CREATE
-    }], 'tracked-SHARE|users-scrapi')
+    }], 'tracked-OPENTRIALS|users-scrapi')
 
 try:
-    users_col = share_ns.create_collection('users', 'tracked-SHARE|users-scrapi')
+    users_col = share_ns.create_collection('users', 'tracked-OPENTRIALS|users-scrapi')
 except jam.exceptions.KeyExists:
     users_col = share_ns.get_collection('users')
 
 schema = dict(
-    schema=dict(
-        type='jsonschema',
-        schema={
-            "id": "/",
-            "type": "object",
-            "properties": {
-                "username": {
-                    "id": "username",
-                    "type": "string",
-                    "pattern": "^\w{1,64}$"
-                },
-                "password": {
-                    "id": "password",
-                    "type": "string",
-                    "pattern": "^\$2b\$1[0-3]\$\S{53}$"
-                }
+    type='jsonschema',
+    schema={
+        "id": "/",
+        "type": "object",
+        "properties": {
+            "username": {
+                "id": "username",
+                "type": "string",
+                "pattern": "^\w{1,64}$"
             },
-            "required": [
-                "username",
-                "password"
-            ]
-        }
-    )
+            "password": {
+                "id": "password",
+                "type": "string",
+                "pattern": "^\$2b\$1[0-3]\$\S{53}$"
+            }
+        },
+        "required": [
+            "username",
+            "password"
+        ]
+    }
 )
 
 share_ns.update('users', [{'op': 'add', 'path': '/schema', 'value': schema}], 'system')
