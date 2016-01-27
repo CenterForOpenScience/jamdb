8186dce2d1c6805eca783a2edc763a55fa33e319
Chris Seto <chriskseto@gmail.com>
2016-01-27 12:54:50 -0500

    Handle missing schema types

--- jam/schemas/__init__.py
+++ jam/schemas/__init__.py
@@ -1,11 +1,16 @@
 from stevedore import driver
 
+from jam import exceptions
+
 
 def load_schema(name, *args, **kwargs):
-    return driver.DriverManager(
-        'jam.schemas',
-        name,
-        invoke_on_load=True,
-        invoke_args=args,
-        invoke_kwds=kwargs,
-    ).driver
+    try:
+        return driver.DriverManager(
+            'jam.schemas',
+            name,
+            invoke_on_load=True,
+            invoke_args=args,
+            invoke_kwds=kwargs,
+        ).driver
+    except driver.NoMatches:
+        raise exceptions.NoSuchSchema(name)
