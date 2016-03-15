402d7da82d2fe7fbbb94f11aa2d7e48d3fde816f
Chris Seto <chriskseto@gmail.com>
2016-03-15 13:09:57 -0400

    Use aiohttps async context manager

--- jam/auth/providers/osf.py
+++ jam/auth/providers/osf.py
@@ -9,10 +9,9 @@ class OSFAuthProvider(BaseAuthProvider):
     type = 'user'
 
     async def _authenticate(self, data):
-        resp = await aiohttp.request('GET', 'https://staging-accounts.osf.io/oauth2/profile', headers={
+        async with aiohttp.request('GET', 'https://staging-accounts.osf.io/oauth2/profile', headers={
             'Authorization': 'Bearer {}'.format(data.get('access_token'))
-        })
-
-        if resp.status != 200:
-            raise exceptions.Unauthorized()
-        return 'user', 'osf', (await resp.json())['id']
+        }) as resp:
+            if resp.status != 200:
+                raise exceptions.Unauthorized()
+            return 'user', 'osf', (await resp.json())['id']
