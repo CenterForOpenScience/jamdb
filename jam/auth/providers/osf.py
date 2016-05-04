c00d91c3ff044db69b5ef3a7260ed88f161437cf
Chris Seto <chriskseto@gmail.com>
2016-05-04 10:38:15 -0400

    Make OSF auth configurable

--- jam/auth/providers/osf.py
+++ jam/auth/providers/osf.py
@@ -1,5 +1,6 @@
 import aiohttp
 
+from jam import settings
 from jam import exceptions
 from jam.auth.providers.base import BaseAuthProvider
 
@@ -9,7 +10,7 @@ class OSFAuthProvider(BaseAuthProvider):
     type = 'user'
 
     async def _authenticate(self, data):
-        async with aiohttp.request('GET', 'https://staging-accounts.osf.io/oauth2/profile', headers={
+        async with aiohttp.request('GET', '{}/oauth2/profile'.format(settings.OSF['ACCOUNTS_URL']), headers={
             'Authorization': 'Bearer {}'.format(data.get('access_token'))
         }) as resp:
             if resp.status != 200:
