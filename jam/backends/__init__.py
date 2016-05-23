302a76ec4a0b767a1aab3653aebaafacf7e73bea
Chris Seto <chriskseto@gmail.com>
2016-05-23 10:42:27 -0400

    Prevent backends from loading prematurely

--- jam/backends/__init__.py
+++ jam/backends/__init__.py
@@ -1,18 +1,18 @@
 from jam.backends.order import O
 from jam.backends.query import Q
-from jam.backends.git import GitBackend
+# from jam.backends.git import GitBackend
 # from jam.backends.redis import RedisBackend
-from jam.backends.mongo import MongoBackend
-from jam.backends.ephemeral import EphemeralBackend
-from jam.backends.elasticsearch import ElasticsearchBackend
+# from jam.backends.mongo import MongoBackend
+# from jam.backends.ephemeral import EphemeralBackend
+# from jam.backends.elasticsearch import ElasticsearchBackend
 
 
 __all__ = (
     'Q',
     'O',
-    'GitBackend',
-    'MongoBackend',
+    # 'GitBackend',
+    # 'MongoBackend',
     # 'RedisBackend',
-    'EphemeralBackend',
-    'ElasticsearchBackend'
+    # 'EphemeralBackend',
+    # 'ElasticsearchBackend'
 )
